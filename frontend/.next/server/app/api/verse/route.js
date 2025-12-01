38331de7955996d852bb8104678aa806c26f6246
Mario Lafuente <mario.l@numeto.com>
2025-12-01 15:45:19 -0600

    testing

---
+++ frontend/.next/server/app/api/verse/route.js
@@ -0,0 +1,6 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/verse/route.js")
+R.c("server/chunks/[root-of-the-server]__2dcada7c._.js")
+R.c("server/chunks/[root-of-the-server]__b2da729a._.js")
+R.c("server/chunks/_next-internal_server_app_api_verse_route_actions_3d976662.js")
+R.m(87267)
+module.exports=R.m(87267).exports
